c5f9903d4baaecb817df7dd62082d7e0204980a2
Andreas Ntaflos <andreas.ntaflos@rise-world.com>
2012-07-09 16:04:35 +0200

    opsview: new puppet type "terminals_monitored"

---
+++ lib/puppet/type/terminals_monitored.rb
@@ -0,0 +1,60 @@
+Puppet::Type.newtype(:terminals_monitored) do
+  @doc = "Monitors the terminal from an Opsview monitoring system"
+
+  ensurable
+
+  newparam(:name, :namevar => true) do
+  end
+
+  newparam(:reload_opsview) do
+    desc "True if you want an Opsview reload to be performed when the host is updated"
+  end
+
+  newproperty(:hostgroup) do
+    desc "Opsview hostgroup"
+  end
+
+  newproperty(:ip) do
+    desc "Node IP address or name"
+  end
+
+  newproperty(:hosttemplates, :array_matching => :all) do
+    desc "Array of Opsview host templates that should be applied to this node"
+  end
+
+  newproperty(:servicechecks, :array_matching => :all) do
+    desc "Array of Opsview service checks that should be applied to this node"
+  end
+  
+  newproperty(:keywords, :array_matching => :all) do
+    desc "Array of Opsview keywords should be applied to this node"
+  end
+  
+  newproperty(:monitored_by) do
+    desc "The Opsview server that monitors this node"
+  end
+  
+  newproperty(:parents, :array_matching => :all) do
+    desc "Array of parents for this node"
+  end
+
+  autorequire(:opsview_hostgroup) do
+    [self[:hostgroup]]
+  end
+
+  autorequire(:opsview_hosttemplate) do
+    self[:hosttemplates]
+  end
+
+  autorequire(:opsview_servicecheck) do
+    self[:servicechecks]
+  end
+  
+  autorequire(:terminals_monitored) do
+    self[:parents]
+  end
+  
+  autorequire(:opsview_keyword) do
+    self[:keywords]
+  end
+end
