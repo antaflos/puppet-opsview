297c25e22cadcac4bd990961260e306a50decb07
Andreas Ntaflos <andreas.ntaflos@rise-world.com>
2013-01-10 21:38:42 +0100

    Most modules are now self-contained, using the anchor pattern.
    Ensure that using the ordering operators on modules (i.e. classes with
    subclasses), as in "Class['custom_repo'] -> Class['mymodule']", results in
    custom_repo being declared/applied and finished *before* anything gets done for
    mymodule.  This means a module written in this way is indeed self-contained.
    This is the behaviour one would naively expect from Puppet, but in reality it
    doesn't work that way.  See issue #8040
    (http://projects.puppetlabs.com/issues/8040). A worthwhile discussion on the
    subject can be found here:
    https://groups.google.com/d/topic/puppet-users/6SdcdBtye9A/discussion
    
    There are still some modules in our tree that need refactoring in this regard,
    especially more complex modules like opsview and nagios.

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,5 +1,5 @@
 class opsview::agent {
-    include 'account::virtual'
-    include 'opsview::agent::install', 'opsview::agent::service', 'opsview::agent::config'
-    realize(Account::Systemgroup['nagios'], Account::Systemuser['nagios'])
+  include 'account::virtual'
+  include 'opsview::agent::install', 'opsview::agent::service', 'opsview::agent::config'
+  realize(Account::Systemgroup['nagios'], Account::Systemuser['nagios'])
 }
