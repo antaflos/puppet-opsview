edb392383099a5240e9d238b99e52c3dd07707a2
Stefan Kleindl <stefan.kleindl@rise-world.com>
2013-08-05 14:48:55 +0200

    adding parent property

--- manifests/server/host.pp
+++ manifests/server/host.pp
@@ -2,6 +2,7 @@ class opsview::server::host (
   $hostgroup,
   $hosttemplates = undef,
   $servicechecks = undef,
+  $parents = undef,
 ){
   @@opsview_monitored { $::hostname:
     ensure         => 'present',
@@ -9,6 +10,7 @@ class opsview::server::host (
     hostgroup      => $hostgroup,
     hosttemplates  => $hosttemplates,
     servicechecks  => $servicechecks,
+    parents        => $parents,
     reload_opsview => true,
   }
 }
