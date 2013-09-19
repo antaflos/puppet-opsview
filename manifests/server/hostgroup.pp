5430357115d77c9be967ce4cbc4e59761b3d44a6
Stefan Kleindl <stefan.kleindl@rise-world.com>
2013-09-19 13:21:12 +0200

    adding hostgroup parent

--- manifests/server/hostgroup.pp
+++ manifests/server/hostgroup.pp
@@ -1,11 +1,13 @@
 define opsview::server::hostgroup (
   $ensure = 'present',
+  $parent = undef,
 ){
   require 'opsview::rest_client'
 
   opsview_hostgroup { $title:
     ensure         => $ensure,
     hostgroup      => $title,
+    parent         => $parent,
     reload_opsview => true,
   }
 
